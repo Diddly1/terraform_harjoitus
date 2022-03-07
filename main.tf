c59c9f89169be282aedf9b1f42cc034331278c1f
pietari <pietari.kivinen@awacademy.training>
2022-03-07 16:20:27 +0200

    bleh

--- main.tf
+++ main.tf
@@ -104,3 +104,24 @@ resource "azurerm_virtual_machine_extension" "vme" {
 }
 SETTINGS
 }
+resource "azurerm_network_security_group" "example" {
+  name                = "acceptanceTestSecurityGroup1"
+  location            = var.location_name
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "TCP"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+resource "azurerm_subnet_network_security_group_association" "example" {
+  subnet_id                 = azurerm_subnet.example.id
+  network_security_group_id = azurerm_network_security_group.example.id
+}
