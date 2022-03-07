0c393fc86f76078161a8cb8e9da9e8309b78f769
pietari <pietari.kivinen@awacademy.training>
2022-03-07 15:54:18 +0200

    iltapäivän juttui tehty

--- main.tf
+++ main.tf
@@ -18,27 +18,89 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
-  location = "westeurope"
+  location = var.location_name
 }
 
-resource "azurerm_storage_account" "storageaccount" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = "westeurope"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# Create a virtual network within the resource group
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  resource_group_name = var.resource_group_name
+  location            = var.location_name
+  address_space       = ["10.0.0.0/16"]
+  depends_on = [
+  azurerm_resource_group.rg,
+  ]
+}
+# Create subnet
+resource "azurerm_subnet" "example" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_storage_container" "container" {
-  name                  = var.container_name
-  storage_account_name  = var.storage_account_name
-  container_access_type = "private"
+resource "azurerm_public_ip" "public_ip" {
+  name                = "vm_public_ip"
+  resource_group_name = var.resource_group_name
+  location            = var.location_name
+  allocation_method   = "Dynamic"
 }
 
-resource "azurerm_storage_blob" "blob" {
-  name                   = "testi"
-  storage_account_name   = var.storage_account_name
-  storage_container_name = var.container_name
-  type                   = "Block"
-  source                 = "c:\\temp\\hulk.jpg"
+resource "azurerm_network_interface" "main" {
+  name                = "testinetworkinterface"
+  location            = var.location_name
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "main" {
+  name                  = "testivm01"
+  location              = var.location_name
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.main.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = "pietari"
+    admin_password = var.salasana
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "staging"
+  }
+}
+resource "azurerm_virtual_machine_extension" "vme" {
+  virtual_machine_id         = azurerm_virtual_machine.main.id
+  name                       = "vme"
+  publisher                  = "Microsoft.Azure.Extensions"
+  type                       = "CustomScript"
+  type_handler_version       = "2.0"
+  auto_upgrade_minor_version = true
+
+  settings = <<SETTINGS
+{
+  "commandToExecute": "sudo apt-get update && apt-get install -y apache2 && echo 'hello world' > /var/www/html/index.html"
+}
+SETTINGS
 }
--- variable.tf
+++ variable.tf
@@ -1,12 +1,13 @@
 variable "subscription_id" { }
 variable "tenant_id" { }
+variable "salasana" { }
 
 variable "storage_account_name" {
-  default = "pietaristorage1"
+  default = "pietaristorageacco1"
 }
 
 variable "storage_container_name" {
-  default = "pietari_blob"
+  default = "pietari_blob1"
 }
 
 variable "resource_group_name" {
@@ -14,5 +15,21 @@ variable "resource_group_name" {
 }
 
 variable "container_name" {
-  default = "pietari-container"
+  default = "pietari-container1"
+}
+
+variable "vnet_name" {
+  default = "pietari-vnet1"
+}
+
+variable "subnet_name" {
+  default = "pietari-subnet1"
+}
+
+variable "location_name" {
+  default = "westeurope"
+}
+
+variable "vm_name" {
+  default = "VM01"
 }
